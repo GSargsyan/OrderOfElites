64d5085cbe8cfd5aa9f5d4c01b15576235ddb184
Grigor Sargsyan <g.sargsyan1995@gmail.com>
2023-04-11 11:54:53 +0000

    Structural changes

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+// frontend/src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/*
+// Define the React app
+const App = () => {
+  const [count, setCount] = React.useState(0)
+  const onClick = () => setCount(c => c + 1)
+  return React.createElement('div', null,
+    React.createElement('h1', null, 'The count is ' + count),
+    React.createElement('button', { onClick: onClick }, 'Count'),
+  )
+}
+// Mount the app to the mount point.
+const root = document.getElementById('app')
+ReactDOM.render(React.createElement(App, null, null), root)
+*/
+
+alert('test')
---
+++ frontend/src/modules/Home/index.js
@@ -0,0 +1,1 @@
+alert('workedd again')
